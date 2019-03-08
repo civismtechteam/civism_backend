a2269575d35f84070a3a6419ff2bfd6f4b0d2323
Ryan Hall <hall.ryan.r@gmail.com>
2019-03-08 15:47:53 -0500

    write initial routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,31 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :initial_states do
+      get "scoops/:id", action: :scoops_show
+  end
+
+  resources :scoops, only: %w[ show ] do
+    member do
+      resources :perspectives, only: %w[ index ]
+      resources :facts, only: %w[ index ]
+    end
+  end
+
+  resources :users, only: %w[ show ]
+
+  resources :perspectives, only: %w[ show ] do
+    member do
+      resources :comments, only: %w[ index create ]
+      resources :votes, only: %w[ create ]
+    end
+  end
+
+  resources :facts, only: %w[ show ] do
+    member do
+      resources :comments, only: %w[ index create ]
+      resources :votes, only: %w[ create ]
+    end
+  end
+
+
 end
