6efd98b88449528d551111830802c541ac85f609
Ryan Hall <hall.ryan.r@gmail.com>
2019-04-12 13:39:21 -0400

    add users to comments

--- db/schema.rb
+++ db/schema.rb
@@ -22,9 +22,11 @@ ActiveRecord::Schema.define(version: 20190308201722) do
     t.integer "num_comments", default: 0, null: false
     t.string "commentable_type"
     t.bigint "commentable_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "facts", force: :cascade do |t|
@@ -81,6 +83,7 @@ ActiveRecord::Schema.define(version: 20190308201722) do
     t.index ["voteable_type", "voteable_id"], name: "index_votes_on_voteable_type_and_voteable_id"
   end
 
+  add_foreign_key "comments", "users"
   add_foreign_key "facts", "scoops"
   add_foreign_key "facts", "users"
   add_foreign_key "perspectives", "scoops"
