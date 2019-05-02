f0dc76445d8f101a461e9f275474c0d6e6f9c94c
Ryan Hall <hall.ryan.r@gmail.com>
2019-05-01 20:10:34 -0400

    whitelist columns

--- app/models/perspective.rb
+++ app/models/perspective.rb
@@ -7,6 +7,6 @@ class Perspective < ApplicationRecord
   has_many :comments, as: :commentable, dependent: :destroy
   has_many :votes, as: :voteable, dependent: :destroy
 
-  self.whitelisted_columns = %i[ id user_id body num_upvotes num_downvotes num_comments ]
+  self.whitelisted_columns = %i[ id user_id body num_upvotes num_downvotes num_comments viewpoint ]
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  
+
   has_secure_password
 
   has_many :comments, dependent: :destroy
 
-  self.whitelisted_columns = %i[ id first_name last_name image_path ]
+  self.whitelisted_columns = %i[ id first_name last_name image_path verified ]
 
 end
